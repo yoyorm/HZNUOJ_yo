45937423029c175bdc135a2ac7f01bf0c38f4268
yoyorm <yoyormiau1011@gmail.com>
2025-09-13 19:18:34 +0800

    添加项目文件。

---
+++ OJ test/1001.c
@@ -0,0 +1,13 @@
+//#include<stdio.h>
+//int main()
+//{
+//	int T;
+//	scanf_s("%d", &T);
+//	long long x, y;
+//	for (int i=0; i < T; i++)
+//	{
+//		scanf_s("%lld%lld", &x,&y);
+//		printf("%lld\n", x + y);
+//	}
+//	return 0;
+//}
---
+++ OJ test/1929.c
@@ -0,0 +1,21 @@
+//Input
+//
+//矩阵的宽n（0 < n < 10）和长m（0 < m < 10）。
+//
+//	Output
+//
+//	以 * 号表示，请打印出矩形。
+//
+//	Samples
+//
+//	input
+//	1 3
+//	output
+//	***
+
+#include<stdio.h>
+int main()
+{
+
+
+}
