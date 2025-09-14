e9500459f4ff6fec345f87f59ffedfbc3171cbfd
yoyorm <yoyormiau1011@gmail.com>
2025-09-14 22:53:27 +0800

    更新：增加了 第程序设计基础第二章Pro部分练习

---
+++ OJ test/2359.c
@@ -0,0 +1,64 @@
+//Input
+//
+//输入包含多组数据。
+//
+//每组数据的第一行有一个N（1 <= n <= 100）
+//
+//接下来有N行 每一行 是"True" 或者 "False", 表示第i道题被解决或者未被解决(1 <= i <= n)
+//
+//Output
+//
+//对于每组输入，输出包含一个数，为解决的题目总数。
+//
+//Samples
+//
+//input
+//2
+//True
+//False
+//1
+//False
+//output
+//1
+//0
+
+//#include<stdio.h>
+//void clean();
+//int main()
+//{
+//	int T;
+//	
+//	
+//	while ((scanf_s("%d", &T)==1)&&T)
+//	{
+//		getchar();
+//		int finish = 0;
+//		for (int i = 0; i < T; i++)
+//		{
+//			char temp;
+//			if ((temp = getchar()) == 'T')
+//			{
+//				finish += 1;
+//				clean();
+//			}
+//			else
+//			{
+//				clean();
+//			}
+//		}
+//		printf("%d\n", finish);
+//		scanf_s("%d", &T);
+//		getchar();
+//	}
+//
+//}
+//
+//void clean()
+//{
+//	char rubbish;
+//	while ((rubbish = getchar()) != '\n' && rubbish != EOF);
+//}
+
+//上述方法太长了
+
+
