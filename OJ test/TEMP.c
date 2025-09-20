a0283c001a536f6f16da0652e5cbcbc53203defb
yoyorm <yoyormiau1011@gmail.com>
2025-09-20 21:13:20 +0800

    更新：完成了程序设计基础第三章部分作业

--- OJ test/TEMP.c
+++ OJ test/TEMP.c
@@ -1,7 +1,17 @@
-//#include<stdio.h>
-//int main()
-//{
-//	
-//	return 0;
-//}
+#include<stdio.h>
+int main()
+{
+	
+	int num = 1;
+	int add=0;
+	while (num <= 100)
+	{
+		if (num % 2 == 0)
+			add += num;
+		num++;
+	}
+	printf("%d", add);
+	return 0;
+}
+
 
