4979c463ab9c9a58084c66f054a3c4cff7a50c8c
yoyormTemp <yoyormiau1011@gmail.com>
2025-10-11 20:23:55 +0800

    更新：完成了程序设计基础第五章部分作业

--- OJ_test/Fundamental_Program_HW/temp.c
+++ OJ_test/Fundamental_Program_HW/temp.c
@@ -1,11 +1,30 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-   float t;
-   int n;
-   scanf("%f%d",&t,&n);
-   float ave=t/n;
-   printf("%.3f\n",ave);
-   printf("%d",n*2);
-   return 0;
+	float data[3];
+	while (scanf("%f%f%f", &data[0], &data[1], &data[2]) == 3)
+	{
+		for (int i = 0; i < 3; i++)
+		{
+			int label = 0;
+			float Min = 190;
+			for (int k = 0; k < 3; k++)
+			{
+				if (data[k] != -1)
+				{
+					if (data[k] < Min)
+					{
+						Min = data[k];
+						label = k;
+					}
+				}
+			}
+			data[label] = -1;
+			if (i < 2)
+				printf("%.1f ", Min);
+			else
+				printf("%.1f\n", Min);
+		}
+	}
+	return 0;
 }
