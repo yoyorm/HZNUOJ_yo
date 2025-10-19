7c748520d86c05cb2ac7cf2db4122876b3bc0bbc
yoyorm <yoyormiau1011@gmail.com>
2025-10-19 17:46:36 +0800

    更新：增加了程序设计基础第六章部分作业 Pro

---
+++ OJ_test/Fundamental_Program_HW/2024ABA.c
@@ -0,0 +1,23 @@
+// Description
+// 无聊的wxa又开始玩字符串了，现在他有一个字符串s,他想知道这个字符串是否含有非重叠AB和BA。(如ABA里AB和BA是重叠的，而ABBA里AB和BA是不重叠的)
+// Input
+// 输入数据有多组。
+// 每组测试数据有一个字符串s。
+// Output
+// 如果可以找到AB和BA，输出”yu ye sa wang dai xing”，否则输出”zhen shi ou ba”
+// Samples
+// input
+// ABA
+// output
+// zhen shi ou ba
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char data[1001];
+    while (scanf("%s", data) != EOF)
+    {
+    }
+}
