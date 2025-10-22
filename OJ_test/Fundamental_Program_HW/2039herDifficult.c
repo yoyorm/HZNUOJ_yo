347a88006ad7e98732cddf53ee640bd25642ea78
yoyormTemp <yoyormiau1011@gmail.com>
2025-10-22 12:56:59 +0800

    更新：完成了程序设计基础第七章作业

--- OJ_test/Fundamental_Program_HW/2039herDifficult.c
+++ OJ_test/Fundamental_Program_HW/2039herDifficult.c
@@ -1,7 +1,21 @@
+// 【C系列7.14】她的困难
+// Description
+//  给你N个数字，求出唯一一个出现次数为奇数的数值。
+// Input
+// 第一行输入一个整数t，代表有多少个数据。
+// 之后每一组测试数据第一行输入一个n（0<n<1000000)，代表n个数字，之后输入n个数字（每一个数字小于10^9）。
+// Output
+// 输出出现次数为奇数的数字。
+// Samples
+// input
+// 1
+// 3
+// 1 1 2
+// output
+// 2
+
 #include <stdio.h>
 
-void verifyData(long long *arr, int size);
-void check(long long arr);
 int main()
 {
     int T;
@@ -10,36 +24,14 @@ int main()
     {
         int num;
         scanf("%d", &num);
-        long long arr = 0;
-        for (int i = 0; i < num; i++)
+        int sum = 0;
+        while (num--)
         {
-            long long size;
-            scanf("%lld", &size);
-            verifyData(&arr, size);
+            int temp;
+            scanf("%d", &temp);
+            sum ^= temp;
+            // 一个数字和自己位异或后为0
         }
-        check(arr);
-    }
-}
-
-void verifyData(long long *arr, int size)
-{
-    *arr ^= (1 << size);
-}
-
-void check(long long arr)
-{
-    int count = 0;
-    while (count < 10e9)
-    {
-        int bit = (arr >> count) & 1;
-        if (bit == 1)
-        {
-            printf("%d", count);
-            return;
-        }
-        else
-        {
-            count++;
-        }
+        printf("%d\n", sum);
     }
 }
