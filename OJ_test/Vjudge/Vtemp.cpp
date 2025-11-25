6abf792226f27cf425514be7e6cc7c5ee0807fb7
yoyormTemp <yoyormiau1011@gmail.com>
2025-11-25 15:39:23 +0800

    更新：增加了ACM程序设计基础作业7 部分

--- OJ_test/Vjudge/Vtemp.cpp
+++ OJ_test/Vjudge/Vtemp.cpp
@@ -1,45 +1,36 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <string>
 using namespace std;
 
-// 把 arr[left..mid] 与 arr[mid+1..right] 合并
-void merge(vector<int> &arr, int left, int mid, int right)
+bool cmp(string a, string b)
 {
-    int n1 = mid - left + 1; // 左段长度
-    int n2 = right - mid;    // 右段长度
-    vector<int> L(arr.begin() + left, arr.begin() + left + n1);
-    vector<int> R(arr.begin() + mid + 1, arr.begin() + mid + 1 + n2);
-
-    int i = 0, j = 0, k = left; // k 写回原数组
-    while (i < n1 && j < n2)
-    {
-        if (L[i] <= R[j])
-            arr[k++] = L[i++]; // 稳定性：<= 取左边
-        else
-            arr[k++] = R[j++];
-    }
-    while (i < n1)
-        arr[k++] = L[i++];
-    while (j < n2)
-        arr[k++] = R[j++];
-}
-void mergeSort(vector<int> &arr, int left, int right)
-{
-    if (left >= right)
-        return;                          // 0 或 1 个元素，天然有序
-    int mid = left + (right - left) / 2; // 防溢出写法
-    mergeSort(arr, left, mid);           // 先排左边
-    mergeSort(arr, mid + 1, right);      // 再排右边
-    merge(arr, left, mid, right);        // 把两段粘成一段有序
+    return a + b > b + a;
 }
 
 int main()
 {
-    vector<int> v = {64, 34, 25, 12, 22, 11, 90, 88, 76, 50, 42, 35, 17, 8, 99, 23, 67, 45, 78, 33};
-    mergeSort(v, 0, v.size() - 1);
-    ;
-    for (int i = 0; i < v.size(); i++)
+    int n;
+    cin >> n;
+    vector<string> s(n);
+    for (int i = 0; i < n; i++)
+        cin >> s[i];
+    sort(s.begin(), s.end(), cmp);
+
+    string t = "";
+    for (int i = 0; i < n; i++)
     {
-        cout << v[i] << " ";
+        t += s[i];
     }
+    while (t.size() > 1)
+    {
+        if (t[0] == '0')
+        {
+            t.erase(0, 1);
+        }
+        else
+            break;
+    }
+    cout << t;
 }
